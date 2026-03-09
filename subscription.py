bf0b00e95a76cf18ce230535e8421ad9f03b64d2
ergashaliyevotabek51-blip <ergashaliyevotabek51@gmail.com>
2026-03-09 21:32:23 +0300

    Create subscription.py

---
+++ subscription.py
@@ -0,0 +1,52 @@
+from database import load_json
+from config import SETTINGS_FILE
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup
+
+def get_channels():
+
+    settings=load_json(SETTINGS_FILE,{"channels":[]})
+
+    return settings.get("channels",[])
+
+async def check_user(context,user_id):
+
+    channels=get_channels()
+
+    result={}
+
+    for ch in channels:
+
+        try:
+
+            member=await context.bot.get_chat_member(ch,user_id)
+
+            if member.status in ["member","administrator","creator"]:
+                result[ch]=True
+            else:
+                result[ch]=False
+
+        except:
+            result[ch]=False
+
+    return result
+
+def keyboard(status):
+
+    kb=[]
+
+    for ch,val in status.items():
+
+        icon="✅" if val else "❌"
+
+        kb.append([
+            InlineKeyboardButton(
+                f"{icon} {ch}",
+                url=f"https://t.me/{ch.replace('@','')}"
+            )
+        ])
+
+    kb.append([
+        InlineKeyboardButton("🔄 Tekshirish",callback_data="check_sub")
+    ])
+
+    return InlineKeyboardMarkup(kb)
