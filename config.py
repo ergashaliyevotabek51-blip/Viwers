7d47e61430717b9ff302ba9c0a4537a56fd66f63
ergashaliyevotabek51-blip <ergashaliyevotabek51@gmail.com>
2026-03-09 21:29:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+BOT_USERNAME = "UzbekFilmTv_bot"
+
+FREE_LIMIT = 5
+REF_LIMIT = 5
+
+USERS_FILE = "users.json"
+MOVIES_FILE = "movies.json"
+SETTINGS_FILE = "settings.json"
+
+DEFAULT_ADMINS = [774440841]
+
+LOG_CHANNEL = None
