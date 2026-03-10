a8eaba17805e4bf121f6ace98c9db30507d541bd
ergashaliyevotabek51-blip <ergashaliyevotabek51@gmail.com>
2026-03-10 06:03:10 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,27 @@
 import time
 
-flood={}
+flood = {}
+COOLDOWN = 1  # sekundlar bilan cheklash, kerak bo'lsa oshirilishi mumkin
 
-def anti_flood(uid):
 
-    now=time.time()
-
+def anti_flood(uid, cooldown=COOLDOWN):
+    """
+    Tez-tez xabar yuborilishini cheklash.
+    Agar foydalanuvchi oxirgi xabardan beri cooldown sekundan kam bo'lsa False qaytaradi.
+    """
+    now = time.time()
     if uid not in flood:
-        flood[uid]=now
+        flood[uid] = now
         return True
-
-    if now-flood[uid]<1:
+    if now - flood[uid] < cooldown:
         return False
-
-    flood[uid]=now
+    flood[uid] = now
     return True
+
+
+def reset_flood(uid):
+    """
+    Foydalanuvchi uchun flood vaqtini reset qiladi
+    """
+    if uid in flood:
+        del flood[uid]
