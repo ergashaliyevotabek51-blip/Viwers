f0dccfc44ef63c53e01f88e79ebf99bc840323af
ergashaliyevotabek51-blip <ergashaliyevotabek51@gmail.com>
2026-03-10 05:57:43 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,15 +1,58 @@
-from database import load_json,save_json
-from config import SETTINGS_FILE,DEFAULT_ADMINS
+from database import load_json, save_json
+from config import SETTINGS_FILE, DEFAULT_ADMINS
+
+
+def get_settings():
+
+    return load_json(SETTINGS_FILE, {
+        "admins": DEFAULT_ADMINS,
+        "channels": []
+    })
+
+
+def save_settings(data):
+
+    save_json(SETTINGS_FILE, data)
+
 
 def get_admins():
 
-    settings=load_json(SETTINGS_FILE,{"admins":DEFAULT_ADMINS})
+    settings = get_settings()
 
     if "admins" not in settings:
-        settings["admins"]=DEFAULT_ADMINS
+        settings["admins"] = DEFAULT_ADMINS
+        save_settings(settings)
 
     return settings["admins"]
+
 
 def is_admin(uid):
 
     return uid in get_admins()
+
+
+def add_admin(uid):
+
+    settings = get_settings()
+
+    if uid not in settings["admins"]:
+        settings["admins"].append(uid)
+        save_settings(settings)
+
+        return True
+
+    return False
+
+
+def remove_admin(uid):
+
+    settings = get_settings()
+
+    if uid in settings["admins"]:
+
+        settings["admins"].remove(uid)
+        save_settings(settings)
+
+        return True
+
+    return False
